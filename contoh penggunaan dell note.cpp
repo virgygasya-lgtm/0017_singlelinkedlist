d7b94a311278d565e0a80b4a86f2b8f2c112f410
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-23 15:55:14 +0700

    contoh penggunaan dell note.cpp

--- contoh penggunaan dell note.cpp
+++ contoh penggunaan dell note.cpp
@@ -78,3 +78,20 @@ bool search(int nim, Node *previous, Node *current)
     return current != NULL;
 }
 
+bool delNode(int nim)
+{
+    Node *current, *previous;
+
+    if (!search(nim, previous, current))
+        return false;
+
+    if (current == START)
+        START = START->next;
+    else
+        previous->next = current->next;
+
+    delete current;
+    return true;
+}
+
+
