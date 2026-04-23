55593b5fe1c3fb6062dd3cab08a188b6fbfc8fbc
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-23 15:48:29 +0700

    contoh penggunaan previous dan node current.cpp

--- contoh penggunaan previous dan node current.cpp
+++ contoh penggunaan previous dan node current.cpp
@@ -58,9 +58,23 @@ while (current != NULL && nim > current->noMhs)
 nodeBaru->next = current;
 previous->next = nodeBaru;
 }
-...
 
 bool listEmpty()
 {
     return START == NULL;
 }
+
+bool search(int nim, Node *previous, Node *current)
+{
+    previous = START;
+    current = START;
+
+    while (current != NULL && nim != current->noMhs)
+    {
+        previous = current;
+        current = current->next;
+    }
+
+    return current != NULL;
+}
+
